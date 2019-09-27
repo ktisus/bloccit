81b47697906723c22dbdd8ed07e8ff275e8a8c32
ktisus <140302003@svuca.edu>
2019-09-26 17:01:23 -0700

    assignment_Intro_Express completed

--- spec/static_spec.js
+++ spec/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -11,13 +12,20 @@ describe("routes : static", () => {
     it("should return status code 200", (done) => {
 
 //#3
-      request.get(base, (err, res, body) => {
+      request.get(marco, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
 //#4
         done();
       });
     });
+
+    it("should return 'polo'", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(body).toBe('polo');
+        done();
+      })
+    })
 
   });
 });
